d7dce9930e11740701372b92bc2b4d8dfa6c3169
Thrifty Kapila <thriftygsf@gmail.com>
2020-10-04 19:43:19 +0000

    :boom: issue with a testcase - trying to fix

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-def add2(l1,l2):
-  diff = (len(l1)-len(l2))
-  if diff > 0:
-    print(l2)
-    for i in range(diff):
-      l2.insert(0,0)
-      print(l2)
-  elif diff<0:
-    print(l1)
-    for i in range(abs(diff)):
-      l1.insert(0,0)
-      print(l1)
+
+# def add2(l1,l2):
+#   diff = (len(l1)-len(l2))
+#   if diff > 0:
+#     print(l2)
+#     for i in range(diff):
+#       l2.insert(0,0)
+#       print(l2)
+#   elif diff<0:
+#     print(l1)
+#     for i in range(abs(diff)):
+#       l1.insert(0,0)
+#       print(l1)
   
-add2([1,2,3],[1,2,3,4])
+# add2([1,2,3],[1,2,3,4])
 
 
 # num = 89
@@ -22,9 +23,19 @@ add2([1,2,3],[1,2,3,4])
 def addTwoNumbers( l1, l2):
   r1 = l1[::-1]
   r2 = l2[::-1]
-  print(r1,r2)
+  diff = (len(r1)-len(r2))
+  if diff > 0:
+    print("Shorter List",r2)
+    for i in range(diff):
+      r2.insert(0,0)
+    print("adding these 2 lists",r1,r2)
+  elif diff<0:
+    print("Shorter List",r1)
+    for i in range(abs(diff)):
+      r1.insert(0,0)
+    print("adding these 2 lists",r1,r2)
   res = [r1[i] + r2[i] for i in range(len(r1))]
-  print(res)
+  print("addition without carry ",res)
   #start i from i+1
   
   for i in range(1,len(res)):
@@ -33,7 +44,9 @@ def addTwoNumbers( l1, l2):
       res[i]=res[i]%10
       res[i-1]+=res1      
       # print(res[i-1],res[i],res1) 
-  return res
+  print("addition with carry ",res) 
+  return res[::-1]
 
-print(addTwoNumbers([2,4],[5,6,4]))
+# print(addTwoNumbers([2,4,3],[5,6,4]))
+print("addition with carry after reversing ",addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]))
 
