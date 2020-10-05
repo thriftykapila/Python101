7cd7bf450459da3d0675443909aecec3d693c44a
Thrifty Kapila <thriftygsf@gmail.com>
2020-10-05 19:10:00 +0000

    :rocket: Palindrome Number

--- Easy/palindromeNumber.py
+++ Easy/palindromeNumber.py
@@ -1,5 +1,3 @@
 def isPalindrome( x):
-  # x1=str(x)
   return (True if str(x)==str(x)[::-1] else False)
-
 print(isPalindrome(12321))
--- Medium/add2Numbers.py
+++ Medium/add2Numbers.py
@@ -1,14 +1,20 @@
-# num = 89
-# w=num/10                = 8.9
-# x=int(num/10)           =  8
-# y=num%10                =  9
 
 def addTwoNumbers( l1, l2):
   r1 = l1[::-1]
   r2 = l2[::-1]
-  print(r1,r2)
+  diff = (len(r1)-len(r2))
+  if diff > 0:
+    print("Shorter List",r2)
+    for i in range(diff):
+      r2.insert(0,0)
+    print("adding these 2 lists",r1,r2)
+  elif diff<0:
+    print("Shorter List",r1)
+    for i in range(abs(diff)):
+      r1.insert(0,0)
+    print("adding these 2 lists",r1,r2)
   res = [r1[i] + r2[i] for i in range(len(r1))]
-  print(res)
+  print("addition without carry ",res)
   #start i from i+1
   
   for i in range(1,len(res)):
@@ -16,14 +22,14 @@ def addTwoNumbers( l1, l2):
       res1 = int(res[i]/10)
       res[i]=res[i]%10
       res[i-1]+=res1      
-      # print(res[i-1],res[i],res1) 
-  return res
+      print(res[i-1],res[i],res1) 
+  print("addition with carry ",res) 
+  return res[::-1]
 
-print(addTwoNumbers([2,4,7],[5,6,4]))
+# print(addTwoNumbers([2,4,3],[5,6,4]))
+print("addition with carry after reversing ",addTwoNumbers([9,9,9,9],[9,9]))
 
 
-
-# logic to equalize two list of different lengths before adding
 
 # def add2(l1,l2):
 #   diff = (len(l1)-len(l2))
@@ -39,3 +45,9 @@ print(addTwoNumbers([2,4,7],[5,6,4]))
 #       print(l1)
   
 # add2([1,2,3],[1,2,3,4])
+
+
+# num = 89
+# w=num/10                = 8.9
+# x=int(num/10)           =  8
+# y=num%10                =  9
