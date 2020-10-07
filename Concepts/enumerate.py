0df4c7c2d74f5cacf042ebd9f8686a49462312b7
Sean Peng <hsinspeng@gmail.com>
2020-10-08 04:21:04 +0800

    Add comments and modify the size of indent.
    It's really a bad practice to use 2 space indent in Python.

--- Concepts/enumerate.py
+++ Concepts/enumerate.py
@@ -1,7 +1,17 @@
-grocery = ['bread', 'milk', 'butter']
-grocery1 = []
-for count, item in enumerate(grocery):
-  grocery1.append(item[::-1])
-  print(item)
+# Python provide the built-in function enumerate() for keeping the count while doing iterations.
+# Let's see its syntax:
+# 
+# enumerate(iterable, start = 0)
+# Parameters:
+# - iterable: any object that supports iteration.
+# - start: the index value from which the counter is to be started, by default it's 0 .
+
+items = ['bread', 'milk', 'butter']
+
+grocery = []
+for count, item in enumerate(items):
+    grocery.append(item[::-1])
+    print(item)
+
+print(items)
 print(grocery)
-print(grocery1)
