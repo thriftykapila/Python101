4bda747d08405f0016f2036473c0ecf1bfab8b4e
HINAL SRIVASTAVA <hinalsri@gmail.com>
2020-10-08 00:29:54 +0530

    Updated Concepts (#8)

---
+++ Concepts/inheritance.py
@@ -0,0 +1,32 @@
+'''
+Contributed by @Hinal-Srivastava
+
+Syntax:
+    class BaseClass:
+        Body of base class
+    class DerivedClass(BaseClass):
+        Body of derived class
+'''
+
+class Employee:
+
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+
+dev_1 = Employee('Hinal', 'Employer', 50000)
+dev_2 = Employee('Larry', 'Employee', 60000)
+
+print(dev_1.email)
+print(dev_2.email)
