57a781f2870dbe5f343316c1539a13817548a785
Him_Modi <36569727+Himanshu-Modi@users.noreply.github.com>
2020-10-14 12:09:33 +0530

    Added Link to Leetcode Problem

--- Leetcode/Easy/Majority_Element.py
+++ Leetcode/Easy/Majority_Element.py
@@ -1,3 +1,5 @@
+
+#https://leetcode.com/problems/majority-element/
 ''' 
 Given an array of size n, find the majority element. 
 The majority element is the element that appears more than ⌊ n/2 ⌋ times.
